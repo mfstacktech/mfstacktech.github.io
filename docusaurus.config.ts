d445974642f13229e9bc0a305e9587fcd44bfcd2
Rajiv Seelam <rajiv@betalectic.com>
2025-02-11 15:09:20 +0530

    Add concurrency to deployment workflow and comment out blog link in Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -128,7 +128,7 @@ const config: Config = {
         //   position: "left",
         //   label: "Tutorial",
         // },
-        { to: "/blog", label: "Blog", position: "left" },
+        // { to: "/blog", label: "Blog", position: "left" },
         // {
         //   href: "https://github.com/facebook/docusaurus",
         //   label: "GitHub",
